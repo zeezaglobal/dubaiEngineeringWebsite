305150dcd094fbf6d0b43dd3cef20f51326bc2c6
Athul Babu <athul.6560@gmail.com>
2025-08-11 00:50:07 -0500

    change height

--- src/subpage/BannerSection.jsx
+++ src/subpage/BannerSection.jsx
@@ -4,27 +4,21 @@ import "./BannerSection.css";
 
 const images = [
   "/src/assets/solidworks.jpeg",
-  "/images/banner2.jpg",
-  "/images/banner3.jpg",
-  "/images/banner4.jpg",
+  "/src/assets/solidworks.jpeg",
+  "/src/assets/solidworks.jpeg",
+  "/src/assets/solidworks.jpeg",
 ];
 
-const contentStyle = {
-  height: "50vh",
-  textAlign: "center",
-  background: "#364d79",
-  lineHeight: "160px",
-};
+
 
 const BannerSection = () => (
-  <div className="banner-section scroll-section">
+  <div className="banner-section">
     <Carousel autoplay className="banner-carousel">
       {images.map((src, index) => (
         <div key={index}>
           <img
             src={src}
             alt={`Banner ${index + 1}`}
-            style={{ width: "100%", height: "160px", objectFit: "cover" }}
           />
         </div>
       ))}
